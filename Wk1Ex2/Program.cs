89bb0d1d660235eb5c7f244dd86f7e280cf03fac
regan <regan@DESKTOP-DH00I47>
2025-01-19 21:26:11 -0600

    Add project files.

---
+++ Wk1Ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine("What is your last name?");
+string lastName = Console.ReadLine();
+Console.WriteLine("What is your age?");
+string age = Console.ReadLine();
+Console.WriteLine("What is your favorite color?");
+string favColor = Console.ReadLine();
+Console.WriteLine("Hello, " + firstName + " " + lastName + "! You are " + age + " years old and your favorite color is " + favColor + ".");
+
